1b4d8e27ab23dac726a227336846a02607e9a021
Luka Gogichaishvili <lukagogichaishvili15@gmail.com>
2026-06-09 18:56:28 +0400

    Add a generic BaseEntity.cs to a domain layer.

---
+++ TaskManagementSystem.Backend.Domain/Generics/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskManagementSystem.Backend.Domain.Generics
+{
+    /// <summary>
+    /// Represents a base entity from which all other entities must inherit from.
+    /// </summary>
+    public class BaseEntity
+    {
+        public Guid Id {  get; set; }
+        public BaseEntity(Guid id)
+        {
+            this.Id = id;
+        }
+    }
+}
