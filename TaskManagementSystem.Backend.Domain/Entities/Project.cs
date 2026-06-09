3cc228eb04e5627c272f68fcd5c0e4fcbf8fa34c
Luka Gogichaishvili <lukagogichaishvili15@gmail.com>
2026-06-09 18:58:56 +0400

    Add Project.cs to Domain/Entities

---
+++ TaskManagementSystem.Backend.Domain/Entities/Project.cs
@@ -0,0 +1,17 @@
+﻿using TaskManagementSystem.Backend.Domain.Generics;
+
+namespace TaskManagementSystem.Backend.Domain.Entities
+{
+    public class Project : BaseEntity
+    {
+        public Project(Guid id) : base(id)
+        {
+        }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public Guid CreatorUserId { get; set; } // ვივარაუდე, რომ შემქმნელის ვინაობა უნდა იქნეს დამახსოვრებული
+        // ნავიგაციის ფროფერთიები
+        public User CreatorUser { get; set; } = default!;
+        public List<TaskItem> Tasks { get; set; } = new();
+    }
+}
