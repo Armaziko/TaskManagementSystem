1f0e829c63f7afbff1a59a17ad5bae85c6bb807b
Luka Gogichaishvili <lukagogichaishvili15@gmail.com>
2026-06-09 20:12:28 +0400

    Modify name and use method overloading for simplicity.

--- TaskManagementSystem.Backend.Api/Extensions/ControllerExtensions.cs
+++ TaskManagementSystem.Backend.Api/Extensions/ControllerExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using TaskManagementSystem.Backend.Application.Models;
 
 namespace TaskManagementSystem.Backend.Api.Extensions
@@ -16,7 +17,7 @@ namespace TaskManagementSystem.Backend.Api.Extensions
             };
         }
 
-        public static IActionResult GetActionResultWithValue<TValue>(this ControllerBase controller, Result<TValue> result)
+        public static IActionResult GetActionResult<TValue>(this ControllerBase controller, Result<TValue> result)
         {
             return result.OperationStatus switch
             {
