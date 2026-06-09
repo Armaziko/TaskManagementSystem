c43d4c76a808655499057768adf46ca30bed8784
Luka Gogichaishvili <lukagogichaishvili15@gmail.com>
2026-06-09 20:11:09 +0400

    Add crucial cueries for CQRS that use mediatR's IRequest

---
+++ TaskManagementSystem.Backend.Application/Queries/GetAllUsersQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TaskManagementSystem.Backend.Application.Models;
+using TaskManagementSystem.Backend.Domain.Entities;
+
+namespace TaskManagementSystem.Backend.Application.Queries
+{
+    public class GetAllUsersQuery : IRequest<Result<IReadOnlyList<User>>>
+    {
+    }
+}
---
+++ TaskManagementSystem.Backend.Application/Queries/GetUserById.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using TaskManagementSystem.Backend.Application.Models;
+using TaskManagementSystem.Backend.Application.Models.DTOs;
+
+namespace TaskManagementSystem.Backend.Api.Controllers
+{
+    public class GetUserById : IRequest<Result<UserDto>>
+    {
+        public Guid Id { get; set; }
+    }
+}
