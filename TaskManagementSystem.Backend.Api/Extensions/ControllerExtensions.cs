1d16bf882fc2c5e54726174fe06eb4dc87ceea7d
Luka Gogichaishvili <lukagogichaishvili15@gmail.com>
2026-06-09 19:48:34 +0400

    Add a ControllerExtensions.cs which includes methods that return IActionResult based on which OperationStatus code a Result object has.

---
+++ TaskManagementSystem.Backend.Api/Extensions/ControllerExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagementSystem.Backend.Application.Models;
+
+namespace TaskManagementSystem.Backend.Api.Extensions
+{
+    public static class ControllerExtensions
+    {
+        public static IActionResult GetActionResult(this ControllerBase controller, Result result)
+        {
+            return result.OperationStatus switch
+            {
+                OperationStatus.SUCCESS => controller.Ok(result),
+                OperationStatus.BAD_REQUEST => controller.BadRequest(result),
+                OperationStatus.NOT_FOUND => controller.NotFound(result),
+                _ => controller.StatusCode(500, result)
+            };
+        }
+
+        public static IActionResult GetActionResultWithValue<TValue>(this ControllerBase controller, Result<TValue> result)
+        {
+            return result.OperationStatus switch
+            {
+                OperationStatus.SUCCESS => controller.Ok(result),
+                OperationStatus.BAD_REQUEST => controller.BadRequest(result),
+                OperationStatus.NOT_FOUND => controller.NotFound(result),
+                _ => controller.StatusCode(500, result)
+            };
+        }
+    }
+}
