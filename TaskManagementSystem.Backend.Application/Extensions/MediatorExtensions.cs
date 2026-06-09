769fe35de2d72af88a4c606ffab84349643476f5
Luka Gogichaishvili <lukagogichaishvili15@gmail.com>
2026-06-09 19:39:47 +0400

    Add an IHostApplicationBuilder extension that configures mediator.

---
+++ TaskManagementSystem.Backend.Application/Extensions/MediatorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TaskManagementSystem.Backend.Application.Commands;
+
+namespace TaskManagementSystem.Backend.Application.Extensions
+{
+    public static class MediatorExtensions
+    {
+        public static void SetUpMediatr(this IHostApplicationBuilder builder)
+        {
+            builder.Services.AddMediatR(config => 
+            {
+                config.RegisterServicesFromAssembly(typeof(CreateUserCommand).Assembly);
+            });
+        }
+    }
+}
