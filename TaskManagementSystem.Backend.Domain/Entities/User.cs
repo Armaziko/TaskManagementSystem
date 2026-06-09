20f852029af7ee789f26dfbcc6c92f715a61d3dd
Luka Gogichaishvili <lukagogichaishvili15@gmail.com>
2026-06-09 18:57:25 +0400

    Add User.cs to domain layer.

---
+++ TaskManagementSystem.Backend.Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using TaskManagementSystem.Backend.Domain.Generics;
+
+namespace TaskManagementSystem.Backend.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public User(Guid id) : base(id)
+        {
+        }
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+
+        // ნავიგაციის ფროფერთიები
+        public List<Project> Projects { get; set; } = new();
+        public List<TaskItem> CreatedTasks { get; set; } = new();
+        public List<TaskItem> AssignedTasks { get; set; } = new();
+        public List<Comment> Comments { get; set; } = new();
+    }
+}
