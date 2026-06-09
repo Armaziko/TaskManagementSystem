5d4692e63f252f7840807b8a036da77a917fc2e0
Luka Gogichaishvili <lukagogichaishvili15@gmail.com>
2026-06-09 18:58:05 +0400

    Add TaskItem.cs to Domain/Entities and TaskItemStatuses.cs to Domain/Enums.

---
+++ TaskManagementSystem.Backend.Domain/Entities/TaskItem.cs
@@ -0,0 +1,25 @@
+﻿using TaskManagementSystem.Backend.Domain.Enums;
+using TaskManagementSystem.Backend.Domain.Generics;
+
+namespace TaskManagementSystem.Backend.Domain.Entities
+{
+    public class TaskItem : BaseEntity
+    {
+        public TaskItem(Guid id) : base(id)
+        {
+        }
+        public string Title { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public TaskItemStatuses Status { get; set; } = TaskItemStatuses.TODO;
+        public Guid ProjectId { get; set; }
+        public Guid CreatorUserId { get; set; } // ვივარაუდე, რომ ამტვირთავის ვინაობაც უნდა შეინახოს.
+        public Guid AssignedUserId { get; set; } 
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // ვივარაუდე, რომ შექმნის დროს დაფიქსირება საჭიროა.
+        public DateTime UpdatedAt { get; set; }
+        // ნავიგაციის ფროფერთიები
+        public Project Project { get; set; } = default!;
+        public User AssignedUser { get; set; } = default!;
+        public User CreatorUser { get; set; } = default!;
+        public List<Comment> Comments { get; set; } = new();
+    }
+}
---
+++ TaskManagementSystem.Backend.Domain/Enums/TaskItemStatuses.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManagementSystem.Backend.Domain.Enums
+{
+    public enum TaskItemStatuses
+    {
+        TODO = 0,
+        IN_PROGRESS = 1,
+        FINISHED = 2
+    }
+}
