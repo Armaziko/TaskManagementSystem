8b9f92e67f9a723f851bfd3bf7b90383cf6e8d5e
Luka Gogichaishvili <lukagogichaishvili15@gmail.com>
2026-06-09 18:59:16 +0400

    Add Comment.cs to Domain/Entities

---
+++ TaskManagementSystem.Backend.Domain/Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿using TaskManagementSystem.Backend.Domain.Generics;
+
+namespace TaskManagementSystem.Backend.Domain.Entities
+{
+    public class Comment : BaseEntity
+    {
+        public Comment(Guid id) : base(id)
+        {
+        }
+        public string Content { get; set; } = default!;
+        public Guid TaskId { get; set; }
+        public Guid CommentMakerId { get; set; } // ვივარაუდე, რომ ნებისმიერ მომხმარებელს შეუძლია კომენტარის გაკეთება
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        // ნავიგაციის ფროფერთიები
+        public TaskItem Task { get; set; } = default!;
+        public User CommentMaker { get; set; } = default!;
+    }
+}
