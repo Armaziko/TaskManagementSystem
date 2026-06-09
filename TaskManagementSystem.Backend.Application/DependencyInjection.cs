28bfd7d7c2c56e7d9f314223bcabdf8e6be76da2
Luka Gogichaishvili <lukagogichaishvili15@gmail.com>
2026-06-09 19:39:13 +0400

    Add Dependency Injection extension that calls an IHostApplicationBuilder extension that sets up mediator.

---
+++ TaskManagementSystem.Backend.Application/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Hosting;
+using TaskManagementSystem.Backend.Application.Extensions;
+
+namespace TaskManagementSystem.Backend.Application
+{
+    public static class DependencyInjection
+    {
+        public static void SetUpApplication(this IHostApplicationBuilder builder)
+        {
+            builder.SetUpMediatr();
+        }
+    }
+}
