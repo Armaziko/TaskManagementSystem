3154ae1142b6504dae2419bee6a1cd1ca5443f05
Luka Gogichaishvili <lukagogichaishvili15@gmail.com>
2026-06-09 19:27:46 +0400

    Add a generic result operation status enum.

---
+++ TaskManagementSystem.Backend.Application/Models/OperationStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagementSystem.Backend.Application.Models
+{
+    public enum OperationStatus
+    {
+        SUCCESS = 0,
+        NOT_FOUND = 1,
+        BAD_REQUEST = 2,
+        FAILURE = 3,
+        INTERNAL_ERROR = 4
+    }
+}
