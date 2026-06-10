19bbc6cf716b7cc6b214370e9c01551bb0855ff0
Luka Gogichaishvili <lukagogichaishvili15@gmail.com>
2026-06-10 18:48:28 +0400

    Add ApplicationDbContext.

---
+++ TaskManagementSystem.Backend.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagementSystem.Backend.Domain.Entities;
+
+namespace TaskManagementSystem.Backend.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<TaskItem> TaskItems { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
