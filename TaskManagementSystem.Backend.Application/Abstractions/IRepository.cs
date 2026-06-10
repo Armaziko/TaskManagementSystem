5ab0be12d8316cad7e023d52130a3b78679534b4
Luka Gogichaishvili <lukagogichaishvili15@gmail.com>
2026-06-10 18:49:10 +0400

    Add generic repository interface.

---
+++ TaskManagementSystem.Backend.Application/Abstractions/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using TaskManagementSystem.Backend.Domain.Generics;
+
+namespace TaskManagementSystem.Backend.Application.Abstractions
+{
+    /// <summary>
+    /// Represents a generic repository of entity type T that is of type <see cref="AggregateRoot"></see>. 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IRepository<T> where T : AggregateRoot // რათა მივყვეთ სუფთა DDD - ს.
+                                                            // თუკი რამე კონკრეტული არა აგრეგატისთვის იქნება საჭირო რეფო
+                                                            // ცალკე უნდა შეიქმნას.
+    {
+        public Task AddAsync(T item);
+        public Task AddRangeAsync(IEnumerable<T> items);
+        public Task<T?> GetByIdAsync(Guid id);
+        public Task<IReadOnlyList<T>> GetAllAsync();
+        public void Update(T item);
+        public void Remove(T item);
+    }
+}
