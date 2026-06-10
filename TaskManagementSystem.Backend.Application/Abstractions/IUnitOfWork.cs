0d2d4f85685c33196d63a37167ff680883a096a1
Luka Gogichaishvili <lukagogichaishvili15@gmail.com>
2026-06-10 18:49:30 +0400

    Add unit of work interface for unit of work pattern.

---
+++ TaskManagementSystem.Backend.Application/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using TaskManagementSystem.Backend.Domain.Generics;
+
+namespace TaskManagementSystem.Backend.Application.Abstractions
+{
+    /// <summary>
+    /// Represents a single, atomic unit of work.
+    /// </summary>
+    public interface IUnitOfWork : IDisposable
+    {
+        public IRepository<T> Repository<T>() where T : AggregateRoot;
+        public int Commit();
+        public Task<int> CommitAsync();
+    }
+}
